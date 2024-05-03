a0836802a2112ccb79c748650bcbc4b055f03737
Gawer <67889836+ubfw32@users.noreply.github.com>
2024-05-03 11:34:40 -0300

    Update and rename Radians to Degrees.py to Radians into degrees.py

--- Radians to Degrees/Radians into degrees.py
+++ Radians to Degrees/Radians into degrees.py
@@ -15,13 +15,15 @@ def convertir():
 
 
 def opt1():
-    res1 = int(input('Insert degrees to convert to radians'))
+    res1 = int(input('Insert degrees to convert into radians'))
     rad = np.radians
     print(f'{res1}° Degrees are {round(rad(res1), 5)} Radians')
 
+
 def opt2():
-    res2 = int(input('Insert radians to converto to degrees '))
+    res2 = int(input('Insert radians into converto to degrees '))
     deg = np.degrees
     print(f'{res2}° Radians are {round(deg(res2), 4)} Degrees')
+
 
 convertir()

b4f8533e560ac92a95615e193d3a7c412e3c1a35
Gawer <67889836+ubfw32@users.noreply.github.com>
2024-05-03 22:48:08 -0300

    Actualizar Count vowels.py

--- Jr Problems/Count vowels/Count vowels.py
+++ Jr Problems/Count vowels/Count vowels.py
@@ -1,14 +1,4 @@
-''' Count Vowels in a String
 
-In Python, you can create a function that counts the number of vowels
-in a given word. Vowels are the letters A, E, I, O, and U, both uppercase and lowercase.
-The function takes a single word as its parameter and returns the number
- of vowels present in the word.
-
-To achieve this, you can use a for loop to iterate through each character in the word.
-Then, you can check if the character is a vowel using a conditional statement.
-If the character is a vowel, you increment a counter variable. Finally,
-you return the counter variable as the result.'''
 
 word = input('Enter some word to analize: \n')
 w = word.lower()
